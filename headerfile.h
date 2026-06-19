5812ab9ecaecf59386bd937e932b0846f836d562
clvfwe <clvfwe@gmail.com>
2026-06-19 18:13:54 +0900

    Add files via upload

---
+++ headerfile.h
@@ -0,0 +1,5 @@
+void update(int i,int j,int scr[7]);
+void mge_sort(int s,int e,int arr[]);
+int compute_rank(int i,int j,int subj);
+void print_index(int i,int subj);
+int compute_grade(int i,int j,int subj);
--- project.c
+++ project.c
@@ -1,11 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "headerfile.h"
 
 int score[7][40][7];//score[i][j][k] : i반 j번 학생의 k과목 성적
 int student[7],mode;//student[i] : i반 학급의 학생 수
 int a,b,s;
 char subject[7][30] = {"국어","영어","수학","한국사","사회탐구","과학탐구","전체"};
+
+
+int main(){
+    printf("===========================================================\n");
+    printf("성적 분석 프로그램\n");
+
+    for(int i = 1;i <= 6;++i){
+        printf("%d반의 학생 수를 입력해 주세요.\n====>   ",i);
+        scanf("%d",student + i);
+        for(int j = 1;j <= student[i];++j){
+            printf("%d반 %d번 학생의 성적을 입력해 주세요.(국어-영어-수학-한국사-사회탐구-과학탐구)\n====>   ",i,j);
+            for(int k = 0;k < 6;++k){
+                scanf("%d",&score[i][j][k]);
+                score[i][j][6] += score[i][j][k];
+            }
+        }
+    }
+    while(1){
+        printf("\n=============================기능 선택=============================\n");
+        printf("1. 성적 등록 및 수정\n");
+        printf("2. 석차 계산\n");
+        printf("3. 등급 계산\n");
+        printf("4. 학급 성적 지표 계산\n");
+        printf("5. 프로그램 종료\n");
+        scanf("%d",&mode);
+        switch (mode){
+            case 1:
+                printf("성적을 수정할 학번을 입력해주세요.(반-번호)\n===>   ");
+                scanf("%d %d",&a,&b);
+                printf("%d반 %d번 학생의 성적을 입력해주세요.\n(국어-영어-수학-한국사-사회탐구-과학탐구)\n===>   ",a,b);
+                int scr[7];
+                scr[6] = 0;
+                for(int i = 0;i < 6;++i){
+                    scanf("%d",scr + i);
+                    scr[6] += scr[i];
+                }
+                update(a,b,scr);
+                break;
+            case 2:
+                printf("석차를 조회할 학번을 입력해주세요.(반-번호)\n===>   ");
+                scanf("%d %d",&a,&b);
+                printf("조회하고 싶은 과목을 입력해주세요.\n1. 국어\n2. 영어\n3. 수학\n4. 한국사\n5. 사회탐구\n6. 과학탐구\n7. 전체성적\n===>   ");
+                scanf("%d",&s);
+                printf("%d반 %d번 학생의 학급내 %s 과목 석차는 %d입니다.\n",a,b,subject[s - 1],compute_rank(a,b,s - 1));
+                break;
+            case 3:
+                printf("등급을 조회할 학생을 입력해주세요.(반-번호)\n===>   ");
+                scanf("%d %d",&a,&b);
+                printf("조회하고 싶은 과목을 입력해주세요.(반-번호)\n1. 국어\n2. 영어\n3. 수학\n4. 한국사\n5. 사회탐구\n6. 과학탐구\n7. 전체성적\n===>   ");
+                scanf("%d",&s);
+                printf("%d반 %d번 학생의 학급내 %s 과목 등급은 %d등급 입니다.\n",a,b,subject[s - 1],compute_grade(a,b,s - 1));
+                break;
+            case 4:
+                printf("성적 지표를 조회할 학급을 입력해주세요.\n===>   ");
+                scanf("%d",&a);
+                printf("조회하고 싶은 과목을 입력해주세요.\n1. 국어\n2. 영어\n3. 수학\n4. 한국사\n5. 사회탐구\n6. 과학탐구\n7. 전체성적\n===>   ");
+                scanf("%d",&s);
+                print_index(a,s - 1);
+                break;
+            case 5:
+                printf("프로그램을 종료합니다.");
+                goto EXIT;
+                break;
+            default:
+                printf("존재하지 않는 기능입니다.\n다시 선택해주세요.\n===>   ");
+        }
+    }
+    EXIT:
+    return 0;
+}
 
 void update(int i,int j,int scr[7]){
     for(int k=0;k<7;++k){
@@ -85,73 +156,4 @@ int compute_grade(int i,int j,int subj) {
     else if (percentage <= 100) {
         return 5;
     }
-}
-
-int main(){
-    printf("===========================================================\n");
-    printf("성적 분석 프로그램\n");
-
-    for(int i = 1;i <= 6;++i){
-        printf("%d반의 학생 수를 입력해 주세요.\n====>   ",i);
-        scanf("%d",student + i);
-        for(int j = 1;j <= student[i];++j){
-            printf("%d반 %d번 학생의 성적을 입력해 주세요.(국어-영어-수학-한국사-사회탐구-과학탐구)\n====>   ",i,j);
-            for(int k = 0;k < 6;++k){
-                scanf("%d",&score[i][j][k]);
-                score[i][j][6] += score[i][j][k];
-            }
-        }
-    }
-    while(1){
-        printf("\n=============================기능 선택=============================\n");
-        printf("1. 성적 등록 및 수정\n");
-        printf("2. 석차 계산\n");
-        printf("3. 등급 계산\n");
-        printf("4. 학급 성적 지표 계산\n");
-        printf("5. 프로그램 종료\n");
-        scanf("%d",&mode);
-        switch (mode){
-            case 1:
-                printf("성적을 수정할 학번을 입력해주세요.(반-번호)\n===>   ");
-                scanf("%d %d",&a,&b);
-                printf("%d반 %d번 학생의 성적을 입력해주세요.\n(국어-영어-수학-한국사-사회탐구-과학탐구)\n===>   ",a,b);
-                int scr[7];
-                scr[6] = 0;
-                for(int i = 0;i < 6;++i){
-                    scanf("%d",scr + i);
-                    scr[6] += scr[i];
-                }
-                update(a,b,scr);
-                break;
-            case 2:
-                printf("석차를 조회할 학번을 입력해주세요.(반-번호)\n===>   ");
-                scanf("%d %d",&a,&b);
-                printf("조회하고 싶은 과목을 입력해주세요.\n1. 국어\n2. 영어\n3. 수학\n4. 한국사\n5. 사회탐구\n6. 과학탐구\n7. 전체성적\n===>   ");
-                scanf("%d",&s);
-                printf("%d반 %d번 학생의 학급내 %s 과목 석차는 %d입니다.\n",a,b,subject[s - 1],compute_rank(a,b,s - 1));
-                break;
-            case 3:
-                printf("등급을 조회할 학생을 입력해주세요.(반-번호)\n===>   ");
-                scanf("%d %d",&a,&b);
-                printf("조회하고 싶은 과목을 입력해주세요.(반-번호)\n1. 국어\n2. 영어\n3. 수학\n4. 한국사\n5. 사회탐구\n6. 과학탐구\n7. 전체성적\n===>   ");
-                scanf("%d",&s);
-                printf("%d반 %d번 학생의 학급내 %s 과목 등급은 %d등급 입니다.\n",a,b,subject[s - 1],compute_grade(a,b,s - 1));
-                break;
-            case 4:
-                printf("성적 지표를 조회할 학급을 입력해주세요.\n===>   ");
-                scanf("%d",&a);
-                printf("조회하고 싶은 과목을 입력해주세요.\n1. 국어\n2. 영어\n3. 수학\n4. 한국사\n5. 사회탐구\n6. 과학탐구\n7. 전체성적\n===>   ");
-                scanf("%d",&s);
-                print_index(a,s - 1);
-                break;
-            case 5:
-                printf("프로그램을 종료합니다.");
-                goto EXIT;
-                break;
-            default:
-                printf("존재하지 않는 기능입니다.\n다시 선택해주세요.\n===>   ");
-        }
-    }
-    EXIT:
-    return 0;
 }
